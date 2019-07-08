11cfd5760001041a2eabfe2c2a75e553f9455a00
Sachiya-Fujita <s4ch1mos@gmail.com>
2019-07-08 18:30:42 +0900

    cocoapodの設定

---
+++ NetworkTest/NetworkTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkTest
+//
+//  Created by 1st-impact on 2019/07/08.
+//  Copyright © 2019 S4ch1mos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
